d3e913c17228021ffe49565482774e2f0ee221f1
Ítalo Moura <italo.moura1201@gmail.com>
2022-04-25 20:47:34 -0300

    testes

--- cypress/integration/test/qainatel.spec.js
+++ cypress/integration/test/qainatel.spec.js
@@ -12,5 +12,26 @@ describe('visit google', () => {
         cy.get("#username").type("sdfsa")
         cy.get("#password").type("asfsafs")
         cy.get(".btn-primary").click()
+        cy.get(".alert-success").should("contain.text", "Registration successful")
+        // ./node_modules/.bin/cypress open
+    })
+
+    it('Login na plataforma com sucesso!', () => {
+        createUser()
+        cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+        cy.get("input[name=username]").type("sdfsa")
+        cy.get("input[name=password]").type("asfsafs")
+        cy.get(".btn-primary").click()
     })
 })
+
+function createUser(){
+    cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get(".btn-link").click();
+    cy.get("#firstName").type("Ítalo")
+    cy.get("input[name=lastName]").type("Moura")
+    cy.get("#username").type("sdfsa")
+    cy.get("#password").type("asfsafs")
+    cy.get(".btn-primary").click()
+    cy.get(".alert-success").should("contain.text", "Registration successful")
+}
